ea16e606c3b1cc49a07ac954b730895dcb33d601
noodlescodes <noodlescodes@gmail.com>
2013-07-26 15:51:51 +1000

    Adding the files used by the script and the script itself. Initial setup to scrape "Computer" from wikipedia.

---
+++ webScraper.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+
+import json
+import urllib
+import re
+import unicodedata
+from HTMLParser import HTMLParser
+
+#putting variables here doesn't seem right, and I'm sure I can move the constAddr1 and constAddr3 variables
+page = ""
+constAddr1 = "http://en.wikipedia.org/w/api.php?format=json&action=query&titles="
+addr2 = ""
+constAddr3 = "&prop=revisions&rvprop=content"
+htmlText = ""
+data = ""
+lines = ""
+
+def whichPage():
+    global page
+    global constAddr1
+    global constAddr3
+    global addr2
+    global lines
+    lines = open('sitesToGet.txt').readlines()
+    addr2 = lines[0][:-1]
+    page = constAddr1 + addr2 + constAddr3
+
+def getPage():
+    global htmlText
+    global page
+    htmlfile = urllib.urlopen(page)
+    htmlText = htmlfile
+
+#removes the html tags but keeps content between them
+def removeTags(html):
+    global data
+    global htmlText
+    s = MLStripper()
+    s.feed(html)
+    data = s.get_data()
+
+def getContent():
+    global htmlText
+    global data
+    data = json.load(htmlText)
+    data = data["query"]["pages"]
+    data = data[data.keys()[0]]["revisions"]
+    data = data[-1] #I don't even know what's going on here, it works, not changing it now
+    data = data["*"]
+
+#removes everything from the section onwards
+def removeReferences():
+    global data
+    if data.find("==References==") >= 0:
+        data = data[:data.find("==References==") - 1]
+    if data.find("==See also==") >= 0:
+        data = data[:data.find("==See also==") - 1]
+    if data.find("==Notes==") >= 0:
+        data = data[:data.find("==Notes==") - 1]
+
+#removes the infobox because it's rarely natural
+def removeInfobox():
+    global data
+    p = re.compile('\|\s.*')
+    data = re.sub(p, '', data)
+    p = re.compile('\{\{[iI]nfobox.*?\}\}', re.S)
+    data = re.sub(p, '', data)
+
+#removes the cite tags
+def removeCite():
+    global data
+    p = re.compile('\{\{[cC]ite.*?\}\}', re.S)
+    data = re.sub(p, '', data)
+
+#removes wikitables because they are rarely natural
+def removeWikitables():
+    global data
+    p = re.compile('\{\|\sclass=\"wikitable\".*?\|}', re.S)
+    data = re.sub(p, '', data)
+
+#must come after removeReferences
+#removes the heading of a section because it's not natural
+def removeHeadings():
+    global data
+    p = re.compile('==*.*?==*')
+    data = re.sub(p, '', data)
+    p = re.compile('\{\{[mM]ain.*?\}\}')
+    data = re.sub(p, '', data)
+
+#removes File elements, generally describing an image
+def removeFile():
+    global data
+    p = re.compile('\[\[[fF]ile.*?\]\].*?\..*?\]\]')
+    data = re.sub(p, '', data)
+    p = re.compile('\[\[[fF]ile.*?\|thumb\|.*?\]\]')
+    data = re.sub(p, '', data)
+
+#removes what redirects to this page because it's not natural
+def removeRedirected():
+    global data
+    p = re.compile('\{\{.*?\|.*?\}\}')
+    data = re.sub(p, '', data)
+
+#removes reference tags (and everything between) because they are not natural
+def removeRefTags():
+    global data
+    p = re.compile('<ref>.*?</ref>', re.S)
+    data = re.sub(p, '', data)
+
+#removes code tags (and everything between) because they are not natural
+def removeCode():
+    global data
+    p = re.compile('<code>.*?</code>', re.S)
+    data = re.sub(p, '', data)
+
+#remove double open double close {} (and everything between) because the text is rarely natural between them
+def removeBraces():
+    global data
+    p = re.compile('\{\{.*?\}\}')
+    data = re.sub(p, '', data)
+
+#remove certain punctuation marks
+def removePunctuation():
+    global data
+    p = re.compile('[\'\"]')
+    data = re.sub(p, '', data)
+    p = re.compile('\&nbsp\;')
+    data = re.sub(p, ' ', data)
+
+#replaces newline characters and puts everything on a single line. This can be made more efficient
+def removeNewLine():
+    global data
+    p = re.compile('\n')
+    data = re.sub(p, '', data)
+    p = re.compile('\.')
+    data = re.sub(p, '. ', data)
+    p = re.compile('\s\s')
+    data = re.sub(p, ' ', data)
+
+#everything that needs to be removed
+def removals():
+    removeRefTags()
+    removeCode()
+    removeTags(data)
+    removeWikitables()
+    removeInfobox()
+    removeReferences()
+    removeCite()
+    removeBullets()
+    removeHeadings()
+    removeRedirected()
+    removeFile()
+    removeBraces()
+    removePunctuation()
+    removeNewLine()
+
+#finds all the links left on the page after removal, and writes them to file and replaces the link with the correct word in the data
+def getLinksFromPage():
+    global data
+    siteFile = open('sitesToGet.txt', 'a')
+    p = re.compile('\[\[.*?\]\]')
+    rawText = re.search(p, data)
+    while rawText:
+        rawText = rawText.group(0)
+        rawText = rawText[2:]
+        rawText = rawText[:-2]
+        words = rawText.split('|')
+        link = words[0]
+        word = words[len(words) - 1]
+        data = re.sub(p, word, data, 1)
+        siteFile.write(link + "\n")
+        rawText = re.search(p, data)
+    siteFile.close()
+
+#write the resulting data to a file
+#totalDown.txt stores a single number, which is used as a filename
+def savePage():
+    global data
+    f = open('totalDown.txt', 'r')
+    fileNumber = f.readline()[:-1]
+    f.close()
+    fileName = fileNumber + ".txt"
+    f = open(fileName, 'w')
+    f.write(data)
+    f.close()
+    fileNumber = int(fileNumber)
+    fileNumber += 1
+    f = open('totalDown.txt', 'w')
+    f.write(str(fileNumber) + "\n")
+    f.close()
+
+def parse():
+    global data
+    getContent()
+    removals()
+    data = unicodedata.normalize('NFKD', data).encode('ascii', errors='ignore')
+    getLinksFromPage()
+    savePage()
+
+#updates all the necessary files
+#sitesCompleted.txt lists all the sites that have been downloaded
+#sitesToGet.txt lists all the sites that have been found to exist from finding links for them.
+def updateFiles():
+    global lines
+    global addr2
+    f = open('sitesCompleted.txt', 'a')
+    f.write(addr2 + " | " + page + "\n")
+    f.close()
+    lines = open('sitesToGet.txt').readlines()
+    f = open('sitesToGet.txt', 'w')
+    for i in range(1, len(lines) - 1):
+        f.write(lines[i])
+    f.close()
+
+def main():
+    global addr2
+    for i in range(0, 3):
+        whichPage()
+        print "Working on: " + addr2
+        getPage()
+        parse()
+        updateFiles()
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def handle_entityref(self, name):
+        self.fed.append('&%s;' % name)
+    def get_data(self):
+        return ''.join(self.fed)
+
+if __name__ == "__main__":
+    main()

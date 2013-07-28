c80d903be3270dd6501bdeb628e1ef414614e566
noodlescodes <noodlescodes@gmail.com>
2013-07-28 08:33:36 +0000

    changed the file output address and forgot to change it back

--- webScraper.py
+++ webScraper.py
@@ -236,7 +236,7 @@ def savePage():
     f = open('totalDown.txt', 'r')
     fileNumber = f.readline()[:-1]
     f.close()
-    fileName = fileNumber + ".txt" #change this line to save to a different location
+    fileName = "/media/usbstick/wikiPages/" + fileNumber + ".txt" #change this line to save to a different location
     f = open(fileName, 'w')
     f.write(data)
     f.close()
